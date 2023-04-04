68b1e2fc85640f622f9d78f2530410001ced843f
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-04-04 20:38:43 +0300

    easy commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 
-img = cv2.imread('D:/GB/bootcamp/ExploringOpenCV/test.jpg')
+img = cv2.imread('D:\Jack\Java II/test.jpg')
 
-#img = cv2.resize(img, (500, 500))
+img = cv2.resize(img, (300, 500))
 
 cv2.imshow('Result', img)
 
